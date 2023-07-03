7ebe450f00297dfda00a297ec46f10f15eb5aca9
grimaldo.soto <Grimaldo.SOTO@icloud.com>
2023-07-03 09:40:54 -0500

    Clase 5 - Práctica Tipos de Enteros

---
+++ 02TiposIntegrados/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+int i = 123;
+byte a = 0b1111_1111;
+long k = 0xAFD12;
+Console.WriteLine("El valor de i es {0}", i);
+Console.WriteLine("El valor de a es {0}", a);
+Console.WriteLine("El valor de k es {0}", k);
+Console.ReadKey();
