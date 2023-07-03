f8f76bc00c40110103a15b6900d186f65495b46d
grimaldo.soto <Grimaldo.SOTO@icloud.com>
2023-07-03 13:08:55 -0500

    Clase 15 - Registros

---
+++ 06libPedidos/Registros.cs
@@ -0,0 +1,15 @@
+namespace _06libPedidos;
+
+public record class recProductos
+{
+    public int idProducto { get; set; }
+    public string Descripcion { get; set; }
+    public string CodigoBarras { get; set; }
+
+    public recProductos(int id, string descripcion, string codigobarras)
+    {
+        this.idProducto = id;
+        this.Descripcion = descripcion;
+        this.CodigoBarras = codigobarras;
+    }
+}
