618ef3a76c0fbc735656c82fc7c0b286d1d728af
grimaldo.soto <Grimaldo.SOTO@icloud.com>
2023-07-03 12:45:19 -0500

    Clase 14 - Práctica de Clases

--- 06AplicacionPedidos/Program.cs
+++ 06AplicacionPedidos/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hola mundo");
+﻿using _06libPedidos;
+
+clsClientes cliente;
+cliente = new clsClientes();
+cliente.idCliente = 1;
+cliente.NombreCompleto = "Marcos Hernandez";
+cliente.Rfc = "HEH010165";
+Console.WriteLine(cliente.ToString());
