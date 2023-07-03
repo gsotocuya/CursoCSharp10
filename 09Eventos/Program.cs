ef1d8fb2cb93f073d88731603514a7a46b8c10ce
grimaldo.soto <Grimaldo.SOTO@icloud.com>
2023-07-03 16:31:56 -0500

    Clase 24 - Eventos Pt1

---
+++ 09Eventos/Program.cs
@@ -0,0 +1,24 @@
+﻿using _09Eventos;
+
+int opcion = 0;
+do
+{
+    Console.WriteLine("1. Eventos con delegados");
+    Console.WriteLine("10. Salir");
+    opcion = Convert.ToInt32(Console.ReadLine());
+    switch (opcion)
+    {
+        case 1:
+            clsRepositorioDelegado repo = new clsRepositorioDelegado();
+            repo.ReportarAvance += MuestraAvance;
+            repo.ObtenTodos();
+            break;
+        default:
+            break;
+    }
+} while (opcion != 10);
+
+void MuestraAvance(int valor)
+{
+    Console.Write(valor.ToString() + " ");
+}
